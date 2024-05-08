b6dc7f97ca6cd4014b4914c49c031adb07f601e6
CodeVizk <vivek223singh@gmail.com>
2024-05-09 00:27:17 +0530

    updated day 6

---
+++ Day6/DefiningFunction.py
@@ -0,0 +1,71 @@
+# Defining a function
+def my_function():
+  print("Hello World")
+
+# Calling a function
+my_function()
+
+
+################For practising Function try this website ################33 
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_right():
+#     for i in range(0,3):
+#         turn_left()
+    
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for i in range(6):
+#     jump()
+
+
+
+############### Using WHILE loop in Reeborg hurdle 2###############3
+# def turn_right():
+#     for i in range(0,3):
+#         turn_left()
+
+     
+# def jump():
+    
+#         move()
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()       
+# while at_goal() != True:
+#     jump()
+    
+
+
+################ Reeborg hurdle 3################3
+
+# def turn_right():
+#     for i in range(0,3):
+#         turn_left()
+
+# def jump():
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()       
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
---
+++ Day6/Indentation.py
@@ -0,0 +1,7 @@
+# Use 4 spaces for indentation
+# indentation is needed for every function
+
+def add_function(x, y):
+    if 3>2:
+        print(x + y)
+        
