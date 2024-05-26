f91827982cdcd22a4e9980fd91ff8b071ab25a21
CodeVizk <vivek223singh@gmail.com>
2024-05-26 23:17:39 +0530

    Making a calculator function using loops, dictionary, recursion

---
+++ Day10/Calculator.py
@@ -0,0 +1,43 @@
+# Making a Calculator using Loops, Dictionary, Recursion, Function.
+
+
+def calculator():
+  def add(n1,n2):
+    return n1+n2
+
+  def subtract(n1,n2):
+    return n1-n2
+
+  def multiply(n1,n2):
+    return n1*n2
+
+  def divide(n1,n2):
+    return n1/n2
+  operations = {
+    "+":add ,
+    "-":subtract ,
+    "*":multiply ,
+    "/":divide
+  }
+  num1=int(input("What's the first number? "))
+  should_continue=True
+  while should_continue:
+    
+    num2=int(input("What's the second number? "))
+    for key in operations:
+      print(key)
+    User_Operation=str(input("Select operation "))
+    
+    calculation_function=operations[User_Operation]
+    answer=calculation_function(n1=num1,n2=num2)
+    
+    print(f"{num1} {User_Operation} {num2} = {answer}")
+    user_continue=input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start new calculation- ")
+    if user_continue=="y":
+      num1=answer
+      
+    else:
+      should_continue=False
+      calculator()
+
+calculator()
