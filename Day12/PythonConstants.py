d9875336e175bad1d5750132a0f4ad13ed2801cf
CodeVizk <vivek223singh@gmail.com>
2024-05-31 00:40:03 +0530

    How to define a  global variable and it's uses .

---
+++ Day12/PythonConstants.py
@@ -0,0 +1,7 @@
+# We generally use a Global Variable to store a value which is not changed ever again like a constant. So in Python we define a global variable 
+# in CAPITAL letter with underscore so that we remember it is a global variable and not change it by mistake 
+PI = 3.14159
+URL= "https://www.google.co.in/"
+LINKEDIN_HANDLE="https://www.linkedin.com/in/vivek-kumar-singh-895648296"
+
+# 
