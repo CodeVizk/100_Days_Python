8c00b47cdc3cb01cb527c7d1a465f7c8ebb92a47
CodeVizk <vivek223singh@gmail.com>
2024-06-04 13:17:16 +0530

    Learned about OOPS, Class, Object, Turtle module

---
+++ Day16/OOPsInPython.py
@@ -0,0 +1,11 @@
+from turtle import Turtle,Screen
+
+Jim = Turtle()
+print(Jim)
+Jim.shape("turtle")
+Jim.color("violet")
+Jim.forward(230)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
