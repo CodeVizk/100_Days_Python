29be6c96928b8f02ec205c6fa117035512341570
CodeVizk <vivek223singh@gmail.com>
2024-06-06 18:40:16 +0530

    using data, question_model and main module I created a quiz game.On completion, it gives the final score of the quiz.

---
+++ Day17/quiz-game-start/main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+question_bank = []
+for question in question_data:
+
+    q_text = question["question"]
+    q_answer = question["correct_answer"]
+    new_question = Question(q_text, q_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've have completed the quiz\nYour final score is {quiz.score}/{quiz.question_number}")
