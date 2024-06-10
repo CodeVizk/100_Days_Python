717f745e4ff229192e267f69f6750ede6934351e
CodeVizk <vivek223singh@gmail.com>
2024-06-10 19:50:10 +0530

    Learning about turtle graphics.

---
+++ Day18/TurtleBasics.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+# Creating an object
+jimmy = Turtle()
+
+# Changing the shape
+jimmy.shape("turtle")
+
+# Changing the color of turtle
+jimmy.color("blue")
+
+# Moving the turtle in a square
+for i in range(0,4):
+    jimmy.forward(75)
+    jimmy.right(90)
+
+
+
+
+
+
+
+
+
+
+my_screen = Screen()
+my_screen.exitonclick()
