41b28f90427ef7a1426cb0fb0e60a973198e9c79
CodeVizk <vivek223singh@gmail.com>
2024-06-11 20:13:27 +0530

    Learning about modules and various ways to import it.

---
+++ Day18/Modules.py
@@ -0,0 +1,22 @@
+# Import can be of three types
+
+# 1- importing the module and then using its Function, good for few items/variable
+# import turtle
+# jim = turtle.Turtle()
+
+# # 2-Importing specify thing, makes it easier to write
+# from turtle import Turtle
+# kim = Turtle()
+# tim = Turtle()
+
+# # 3- imports everything from the module not recommended by professional, good coders
+# from turtle import *
+# shape("circle")
+
+
+# Modules can be imported with alias.
+import turtle as t
+jimmy = t.Turtle()
+
+
+# Modules can be installed for specific projects only in a virtual environment
