42f264580f146af6e6cdb77ce41cccb8bec1762a
CodeVizk <vivek223singh@gmail.com>
2024-06-16 23:39:47 +0530

    Creates player for Crossroad game.

---
+++ Day23/turtle-crossing/player.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.left(90)
+
+    def move(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(0, new_y)
+
+    def finished(self):
+        if self.ycor() > FINISH_LINE_Y:
+            self.goto(STARTING_POSITION)
+
