c9a1f130d50a9ceff44e7487334eabc9a6e01ed4
CodeVizk <vivek223singh@gmail.com>
2024-06-18 13:15:37 +0530

    Learning about absolute and relative file paths.

---
+++ Day24/FilePath.py
@@ -0,0 +1,6 @@
+# There are two file paths - Absolute path and relative path
+
+# Absolute path is relative to root path i.e local disk c or any user specified directory
+# Relative path is the directory in which the user is currently working on like here 100_Days_Python/Day24/...
+
+# to use relative path use ./ to specify current directory or ../ to access the upper directory of hierarchy
