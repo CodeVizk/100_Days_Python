aab6bf8a3efcf73c38588481f8afdb388ae916ce
CodeVizk <vivek223singh@gmail.com>
2024-06-19 23:04:42 +0530

    Mail merge 2nd method.

--- Day24/Mail Merge Project Start/method2.py
+++ Day24/Mail Merge Project Start/method2.py
@@ -7,6 +7,6 @@ with open("./Input/Letters/starting_letter.txt") as letter_base:
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}", mode="w") as final_letter:
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as final_letter:
             final_letter.write(new_letter)
 
