7646ad3102f90ade5bb55040ad7d0265d6a5cbfc
CodeVizk <vivek223singh@gmail.com>
2024-06-23 23:08:41 +0530

    using list comprehension

---
+++ Day26/ListComprehension.py
@@ -0,0 +1,10 @@
+number = [1, 2, 3]
+new_item = [i+1 for i in number]
+print(new_item)
+
+name = "Vivek"
+name_list = [n for n in name]
+print(name_list)
+
+multiple_item = [i*2 for i in range(1,5)]
+print(multiple_item)
