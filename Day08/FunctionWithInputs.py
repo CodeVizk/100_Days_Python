bf42677ac403337fe274bb61f681e5fac678e190
Vivek Kumar Singh <162419870+CodeVizk@users.noreply.github.com>
2024-06-23 23:11:50 +0530

    fixed grammar.

--- Day08/FunctionWithInputs.py
+++ Day08/FunctionWithInputs.py
@@ -1,4 +1,4 @@
-# Simple fucntion
+# Simple function
 def greet():
   print("Hello how are you?")
   print("What do you want")
@@ -8,24 +8,23 @@ greet()
 
 
 # Function With Input
-
 def greet_with_name(name):
   print(f"Hello how are you {name}")
-  print (f"What do yo want{name}")
+  print (f"What do you want{name}")
 greet_with_name("Vivek\n")
 
 
 
-# Function With Multiple Parameter
+# Function With Multiple Parameters
 
 def greeting(name, location):
     print(f"My name is {name} and i'm from {location}")
 
-greeting("Vivek","Nala Sopara")
+greeting("Ptown", "Nala Sopara")
 
 # Positional argument
-greeting("soho","Delhi")
-greeting("Delhi","Soho")
+greeting("Soho", "Delhi")
+greeting("Delhi", "Soho")
 
 
 # Keyword Argument
