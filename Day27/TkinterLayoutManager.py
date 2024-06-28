74c03bb31bbb97bc8ae15042168d3879b28e8e71
CodeVizk <vivek223singh@gmail.com>
2024-06-28 23:36:14 +0530

    pack,place,grid.

--- Day27/TkinterLayoutManager.py
+++ Day27/TkinterLayoutManager.py
@@ -14,6 +14,7 @@ def button_click():
 window = Tk()
 window.title("GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=30, pady=30)
 
 # label
 my_label = Label(text="I am a Label", font=("Arial", 24, "italic"))
@@ -21,18 +22,18 @@ my_label = Label(text="I am a Label", font=("Arial", 24, "italic"))
 # my_label.pack(side='bottom')
 # my_label.place(x=100, y=150)
 my_label.grid(column=0, row=0)
-
+my_label.config(padx=25, pady=25)
 # Button
 my_button = Button(text="Click me!", command=button_click)
 new_button = Button(text="New_button", command=button_click)
 # my_button.pack()
 my_button.place(x=10, y=90)
-# my_button.grid(column=1, row=1)
-new_button.grid(column=2,row=0)
+my_button.grid(column=1, row=1)
+new_button.grid(column=2, row=0)
 # Entry
 input = Entry(width=10)
 
 # input.pack()
 # input.place(x=70, y=70)
-input.grid(column=2, row=2)
+input.grid(column=3, row=2)
 mainloop()
