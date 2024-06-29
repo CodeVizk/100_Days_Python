4f28e902668800496a68b0a6a790ebb4a89ace45
CodeVizk <vivek223singh@gmail.com>
2024-06-29 21:38:35 +0530

    Pomodoro timer progre

--- Day28/PomodoroProject/main.py
+++ Day28/PomodoroProject/main.py
@@ -1,4 +1,10 @@
 from tkinter import *
+
+
+def button_click():
+    print("Clicked")
+
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -20,10 +26,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-window = Canvas(width=205, height=225, bg=YELLOW, highlightthickness=0)
+window = Canvas(width=215, height=255, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 
-window.create_image(100, 112, image=tomato_img)
-window.create_text(100, 130, text="0:00", fill="white", font=(FONT_NAME, 35, "bold"))
-window.pack()
+window.create_image(100, 130, image=tomato_img)
+window.grid(column=1, row=1)
+
+window.create_text(100, 145, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+window.grid(column=1, row=1)
+
+start_button = Button(text="Start", command=button_click, font=(FONT_NAME, 8, "bold"), highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=button_click, font=(FONT_NAME, 8, "bold"), highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark = Label(text="✓", fg="green", bg=YELLOW, font=10)
+checkmark.grid(column=1, row=3)
+
 window.mainloop()
