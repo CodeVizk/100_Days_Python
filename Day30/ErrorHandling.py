4520e8b1a6037e7f54da8fe5a338cee9590ed2a8
CodeVizk <vivek223singh@gmail.com>
2024-07-01 22:07:39 +0530

    Types of errors and handling it

---
+++ Day30/ErrorHandling.py
@@ -0,0 +1,15 @@
+# File not found
+# with open("text.txt", "r") as file:
+#     file.read()
+
+
+# KeyError
+# a_dictionary = {'key': "value"}
+# value = a_dictionary["non_existent_key"]
+
+
+# Index error
+# fruits = ["apple", "mango", "banana"]
+# print(fruits[3])
+
+
