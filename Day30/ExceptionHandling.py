e445cfee3e04e18ea924b4f4d703214fecdadfe8
CodeVizk <vivek223singh@gmail.com>
2024-07-01 22:20:14 +0530

    Exception Handling.

--- Day30/ExceptionHandling.py
+++ Day30/ExceptionHandling.py
@@ -1,22 +1,22 @@
 # File not found
 
-# try:
-#     file = open("text.txt")
-#     a_dictonary = {"key": "value"}
-#     print(a_dictonary["gerwe"])
-#
-# except FileNotFoundError:
-#     file = open("text.txt", "w")
-#     file.write("xyz")
-#
-# except KeyError as error_message:
-#     print(F"The Key {error_message} does not exist")
-#
-# else:
-#     content = file.read()
-#     print(content)
-#
-# finally:
-#     file.close()
-#     print("File was closed")
-#     raise TypeError("This is an example")
+try:
+    file = open("text.txt")
+    a_dictonary = {"key": "value"}
+    print(a_dictonary["gerwe"])
+
+except FileNotFoundError:
+    file = open("text.txt", "w")
+    file.write("xyz")
+
+except KeyError as error_message:
+    print(F"The Key {error_message} does not exist")
+
+else:
+    content = file.read()
+    print(content)
+
+finally:
+    file.close()
+    print("File was closed")
+    raise TypeError("This is an example")
