8d35ff95598655060ab03c2f58f5560d478e6b98
CodeVizk <vivek223singh@gmail.com>
2024-07-04 22:52:54 +0530

    Flash Card Project.

--- Day31/FlashCardApp/main.py
+++ Day31/FlashCardApp/main.py
@@ -7,13 +7,12 @@ BACKGROUND_COLOR = "#B1DDC6"
 lang_data = pandas.read_csv("data/french_words.csv")
 data_list = lang_data.to_dict(orient="records")
 
-f_words = ""
-
 
 def french_words():
-    global f_words
     f_words = random.choice(data_list)
-    flash_canvas.itemconfigure(words,  text=f"{f_words["French"]}", font=("Ariel", 60, "bold"))
+    flash_canvas.itemconfig(card_title, text="French")
+    flash_canvas.itemconfig(card_word,  text=f"{f_words["French"]}")
+
 
 window = Tk()
 window.title("Flash Card ")
@@ -31,10 +30,10 @@ wrong_button.grid(column=0, row=1)
 flash_card_front = PhotoImage(file="images/card_front.png")
 flash_canvas = Canvas(highlightthickness=0, bg=BACKGROUND_COLOR, width=800, height=526)
 flash_canvas.create_image(400, 263, image=flash_card_front)
-flash_canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-words = flash_canvas.create_text(400, 263, text=f"{f_words}", font=("Ariel", 60, "bold"))
+card_title = flash_canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = flash_canvas.create_text(400, 263, text=f"", font=("Ariel", 60, "bold"))
 flash_canvas.grid(column=0, row=0, columnspan=2)
-
+french_words()
 
 
 
