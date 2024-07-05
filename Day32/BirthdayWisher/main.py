2652445258fde845384a1af87094665e35d4b9eb
CodeVizk <vivek223singh@gmail.com>
2024-07-06 00:30:31 +0530

    Learning SMTP.

--- Day32/BirthdayWisher/main.py
+++ Day32/BirthdayWisher/main.py
@@ -1,11 +1,13 @@
-import smtplib
+##################### Extra Hard Starting Project ######################
 
-my_email = "codevizk@gmail.com"
-password = "vydq ennv bbay ydau"
+# 1. Update the birthdays.csv
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="vivek223singh@gmail.com",
-                        msg="Subject:Hello\n\nFirst SMTP test.")
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
