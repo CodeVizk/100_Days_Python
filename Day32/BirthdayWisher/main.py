546f17b37c4dbbf5765d0c60f4fd601e46abf74a
CodeVizk <vivek223singh@gmail.com>
2024-07-05 23:50:16 +0530

    Birthday wisher in progress.

--- Day32/BirthdayWisher/main.py
+++ Day32/BirthdayWisher/main.py
@@ -3,10 +3,9 @@ import smtplib
 my_email = "codevizk@gmail.com"
 password = "vydq ennv bbay ydau"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs="vivek223singh@gmail.com",
-                    msg="Subject:Hello\n\nFirst SMTP test.")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="vivek223singh@gmail.com",
+                        msg="Subject:Hello\n\nFirst SMTP test.")
