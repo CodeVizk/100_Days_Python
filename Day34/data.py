f5d55f89019af580b425eeec0af4fc33366beacb
CodeVizk <vivek223singh@gmail.com>
2024-07-09 22:09:15 +0530

    gets data using OpenTrivia api

--- Day34/data.py
+++ Day34/data.py
@@ -7,8 +7,5 @@ parameter = {
 data = requests.get("https://opentdb.com/api.php", params=parameter)
 data.raise_for_status()
 questions = data.json()
-# ques = questions["results"][0]
-# print(ques["question"])
-
 question_data = questions["results"]
 
