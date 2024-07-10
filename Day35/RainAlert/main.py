b40d6b59c21dd428260617e4e8391e0e7d2457fd
Vivek Kumar Singh <162419870+CodeVizk@users.noreply.github.com>
2024-07-10 23:11:09 +0530

    Update main.py

--- Day35/RainAlert/main.py
+++ Day35/RainAlert/main.py
@@ -3,7 +3,7 @@ import requests
 MY_LAT = 28.631662
 MY_LONG = 77.194120
 endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "f500d64e87b7c125b5eae2a366a49cfd"
+api_key = "add your own from openweathermap"
 parameter = {
     "lat": MY_LAT,
     "lon": MY_LONG,
