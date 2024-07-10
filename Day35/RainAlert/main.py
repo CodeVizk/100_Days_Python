8fe03aaa5144eb53a70ff910968dd89b0d06290b
CodeVizk <vivek223singh@gmail.com>
2024-07-10 22:55:08 +0530

    Rain alert program

--- Day35/RainAlert/main.py
+++ Day35/RainAlert/main.py
@@ -7,11 +7,16 @@ api_key = "f500d64e87b7c125b5eae2a366a49cfd"
 parameter = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
 
-request = requests.get(endpoint, params=parameter)
-request.raise_for_status()
-
-print(request.json())
+response = requests.get(endpoint, params=parameter)
+response.raise_for_status()
+weather = response.json()
+# day_weather = response.json()["list"][0]["weather"][0]["id"]
+for hour in weather["list"]:
+    chk = hour["weather"][0]["id"]
+    if chk < 700:
+        print("bring an umbrella")
