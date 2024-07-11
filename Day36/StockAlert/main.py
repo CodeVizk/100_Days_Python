b8264474891507927a72d56f2c901da925174c3f
CodeVizk <vivek223singh@gmail.com>
2024-07-11 21:39:46 +0530

    Stock alert

--- Day36/StockAlert/main.py
+++ Day36/StockAlert/main.py
@@ -5,8 +5,8 @@ import smtplib
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-news_key = "4c257e80042a4e11ac45aa4d3f2f40d6"
-stock_key = " DINB3K673RXR2GFK"
+news_key = "YOUR KEY"
+stock_key = "YOUR KEY"
 
 # Use https://www.alphavantage.co/documentation/#daily
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
@@ -29,10 +29,23 @@ news_params = {
 
 def get_news():
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
-    stock_news_title = news_response.json()["articles"][]
-    print(stock_news_title)
+    stock_news = news_response.json()["articles"]
+    stock_article = stock_news[:3]
+
+    formatted_article = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in stock_article]
+
+    user = "YOUR EMAIL"
+    password = "YOUR PASSWORD"
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(user=user, password=password)
+    connection.sendmail(from_addr=user,
+                        to_addrs="RECEIVER",
+                        msg=f"Subject:Stock Update\n\nTSLA:{up_down}{stock_perc}\n{formatted_article}"
+                        )
 
 
+get_news()
 stock_response = requests.get(STOCK_ENDPOINT, params=stock_params)
 stock_response.raise_for_status()
 stock_data = stock_response.json()
@@ -43,43 +56,19 @@ yesterday = str(day.date())
 day_before = dt.datetime.today() - dt.timedelta(2)
 day_before_yesterday = str(day_before.date())
 
-# yesterday_stock = float(stock_data["Time Series (Daily)"][yesterday]["4. close"])
-# before_yesterday_stock = float(stock_data["Time Series (Daily)"][day_before_yesterday]["4. close"])
-# print(yesterday_stock, before_yesterday_stock)
+yesterday_stock = float(stock_data["Time Series (Daily)"][yesterday]["4. close"])
+before_yesterday_stock = float(stock_data["Time Series (Daily)"][day_before_yesterday]["4. close"])
+print(yesterday_stock, before_yesterday_stock)
+
+change_in_stock = yesterday_stock - before_yesterday_stock
+stock_perc = round((change_in_stock/yesterday_stock) * 100)
+
+up_down = None
+if change_in_stock > 0:
+    up_down = "⬆️"
+else:
+    up_down = "⬇️"
 
 
-get_news()
-
-# change_in_stock = abs(yesterday_stock - before_yesterday_stock)
-# stock_perc = round((change_in_stock/yesterday_stock) * 100, 2)
-# # print(stock_perc)
-# if stock_perc >= 5:
-#     get_news()
-
-# STEP 2: https://newsapi.org/
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-
-# 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-# 7. - Use Python slice operator to create a list that contains the first 3 articles.
-# Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-# STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
-
-# 8. - Create a new list of the first 3 article headline and description using list comprehension.
-# 9. - Send each article as a separate message via Twilio.
-
-
-# Optional : Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+if abs(stock_perc) >= 5:
+    get_news()
