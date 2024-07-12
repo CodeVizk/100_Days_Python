34c0b8194a3cc512eff5e2954e88b21d14c618fb
CodeVizk <vivek223singh@gmail.com>
2024-07-12 18:59:31 +0530

    Habit Tracker

--- Day37/HabitTracker/main.py
+++ Day37/HabitTracker/main.py
@@ -1,8 +1,7 @@
 import requests
 import datetime as dt
 
-USER = "codevizk"
-TOKEN = "dwyhe7562gfu"
+
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 
@@ -13,13 +12,21 @@ user_params = {
     "notMinor": "yes"
 }
 
+
+# Creating a user in Pixela
 response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # response.raise_for_status()
 # print(response.text)
 
 
-graph_endpoint = f"{PIXELA_ENDPOINT}/{USER}/graphs"
+# Creating a header for header request
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 
+
+# Creating a Graph
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USER}/graphs"
 graph_config = {
     "id": "g1",
     "name": "Studying graph",
@@ -28,22 +35,30 @@ graph_config = {
     "color": "momiji"
 }
 
-
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
 respon = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(respon.text)
 
 
-# date = dt.datetime.today()
-
+# Adding data to graph
+date = dt.datetime.today().strftime("%Y%m%d")
 pixel_endpoint = f"{graph_endpoint}/{graph_config["id"]}"
+
 post_pixel_config = {
-    "date": f"20240712",
-    "quantity": "2",
+    "date": date,
+    "quantity": input(int("How many hours did you study")),
 }
+
 
 pixel_res = requests.post(url=pixel_endpoint, json=post_pixel_config, headers=headers)
-print(pixel_res.text)
+
+
+# Updating pixel
+# update_pixel_endpoint = f"{pixel_endpoint}/{date}"
+# update_pixel_config = {
+#     "quantity": "1"
+# }
+
+
+# Deleting pixel
+# update_pixel = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
+# delete_pixel = requests.delete(url=update_pixel_endpoint,headers=headers)
+# print(delete_pixel.text)
