16692111e9b7b53da6ef2ed899e6a1639603b979
CodeVizk <vivek223singh@gmail.com>
2024-07-15 21:39:38 +0530

    Flight Deals

--- Day39/FlightDeals/flight_data.py
+++ Day39/FlightDeals/flight_data.py
@@ -1,3 +1,16 @@
+import requests
+from pprint import pprint
+import os
+
+
+SHEET_ENDPOINT = os.environ.get("SHEET_GET")
+
+authorize = {
+    "Authorization": os.environ.get("SHEET_GET_KEY")
+}
+
+
 class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        self.response = requests.get(url=SHEET_ENDPOINT, headers=authorize)
+        pprint(self.response.json())
