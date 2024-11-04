0d8f9ec57bb71bad8f90049238af5e3a2bf36e2e
CodeVizk <vivek223singh@gmail.com>
2024-11-04 20:03:17 +0530

    Make spotify playlist from billboard list.

--- Day46/main.py
+++ Day46/main.py
@@ -1,21 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
-# date = input("Which year you want to travel.Enter the year in YYYY-MM-DD format.")
-date = "2010-08-12"
+CLIENT_ID = "YOUR ID"
+CLIENT_SECRET = "YOUR SECRET "
+
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
-url = "https://www.billboard.com/charts/hot-100/" + date
-response = requests.get(url=url, headers=header)
-billboard_webpage = response.text
+billboard_url = "https://www.billboard.com/charts/hot-100/" + date
+response = requests.get(url=billboard_url, headers=header)
 
-song_soup = BeautifulSoup(billboard_webpage, "html.parser")
-song_names = []
-artists = []
-artist_soup = song_soup.select('div>ul>li>ul>li>span.a-no-trucate.a-font-primary-s')
-# for artist in artist_soup:
-#     artists.append(artist.getText().split())
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names_spans = soup.select("li ul li h3")
+song_names = [song.getText().strip() for song in song_names_spans]
 
-for song in song_soup.select('div>ul>li>ul>li>h3'):
-    song_names.append(song.getText().split())
+# Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
 
+# Searching Spotify for songs by title
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
